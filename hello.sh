da18cb9edcd4d3aa331d48679c1e3dccdf83c08d
cBackup Service <deldslmrtg@gmail.com>
2022-03-10 23:56:13 +0530

    Please_Check_Change_Config

--- hello.sh
+++ hello.sh
@@ -5,12 +5,12 @@ declare -A map
 map["origin"]="delDSL_Backup_Server"
 
 git init
-git checkout -b Master
+git checkout -b master
 git add --a
 git commit -m "Please_Check_Change_Config"
 
 for i in "${!map[@]}"
 do
 	git remote add $i git@github.com:YdvRavi/${map[$i]}.git
-	git push -u $i Master
+	git push -u $i master
 done
